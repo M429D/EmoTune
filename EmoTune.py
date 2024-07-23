38e35ae69faf983ee2d514b64dc03a4a8b69d190
M429D <92875016+M429D@users.noreply.github.com>
2024-07-23 17:50:29 +0800

    Update EmoTune.py

--- EmoTune.py
+++ EmoTune.py
@@ -116,7 +116,6 @@ if st.session_state["genre_selected"] == "initial":
     st.markdown('<div class="key-features">✍🏻 Genre Preferences: Customize your music journey by selecting your preferred instrumental genres for each of the 7 specific emotions, creating a truly personalized touch.\n</div>', unsafe_allow_html=True)
     st.markdown('<div class="key-features">🎭 Emotion Detection: Harness the power of cutting-edge facial emotion recognition algorithms to accurately identify your current emotion in real-time.\n</div>', unsafe_allow_html=True)
     st.markdown('<div class="key-features">🎺 Personalized Music Recommendations: Get instrumental music suggestions tailored to your emotional state, enhancing your listening experience.\n</div>', unsafe_allow_html=True)
-    st.button("Launch")
     
     st.markdown('<div class="section-header">Instrumental Music Genre Preference Customization</div>', unsafe_allow_html=True)
     st.markdown('<div class="key-features">Choose genres to link with the specific emotions, more than 1 may be selected:</div>', unsafe_allow_html=True)
@@ -132,7 +131,7 @@ if st.session_state["genre_selected"] == "initial":
 
     genre_preferences = {}
 
-    with st.form(key='genre_preferences_form'):
+    with st.form(key='genre_preferences_form',):
         st.markdown("<div class='genre-preferences-form'>", unsafe_allow_html=True)
         for emotion in emotions:
             st.markdown(f"<div class='form-header'>{emotion}</div>", unsafe_allow_html=True)
@@ -141,7 +140,7 @@ if st.session_state["genre_selected"] == "initial":
                 with cols[i % 3]:
                     genre_preferences[f"{emotion}_{genre}"] = st.checkbox(f"{genre}", key=f"{emotion}_{genre}")
         st.markdown("</div>", unsafe_allow_html=True)
-        submit_button = st.form_submit_button(label='Submit Preferences')
+        submit_button = st.form_submit_button(label='Launch Now!')
 
     if submit_button:
         st.session_state["genre_selected"] = "customized"

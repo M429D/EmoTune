f30a248cee45a59340d37ace33ee571e0804d2cf
M429D <92875016+M429D@users.noreply.github.com>
2024-11-03 01:57:30 +0800

    Update landing_page.py

--- landing_page.py
+++ landing_page.py
@@ -7,31 +7,13 @@ payment_status = st.query_params.get("payment", [""])  # 'success' or 'fail'
 # Display the appropriate message based on payment status
 if payment_status == "success":
     st.success("Payment success. Thank you for your patronage!")
-    
+   
     countdown_placeholder = st.empty()  # Create a placeholder for the countdown
-    countdown_time = 7  # Start countdown from 7 seconds
+    countdown_time = 30  # Start countdown from 7 seconds
 
     # Countdown loop
     for i in range(countdown_time, 0, -1):
-        countdown_placeholder.write(f"Twilight Coffee Shop is receiving your order in {i} seconds...")
+        countdown_placeholder.write(f"Please close this tab and click 'I have paid' on our website to confirm your transaction within {i} seconds...")
         time.sleep(1)  # Sleep for 1 second
-
-    # After countdown, show the order received message
-    countdown_placeholder.write("Order has been received in the kitchen... You may close this tab now to view your order summary.")
-
-elif payment_status == "fail":
-    st.error("Payment failed. Please try again or contact support.")
-    
-    countdown_placeholder = st.empty()  # Create a placeholder for the countdown
-    countdown_time = 7  # Start countdown from 7 seconds
-
-    # Countdown loop
-    for i in range(countdown_time, 0, -1):
-        countdown_placeholder.write(f"Redirecting user back to login page {i} seconds...")
-        time.sleep(1)  # Sleep for 1 second
-
-    # After countdown, show the order received message
-    countdown_placeholder.write(f"<meta http-equiv='refresh' content='0; url=http://localhost:8501/'>", unsafe_allow_html=True)
-
 else:
     st.warning("No payment status provided.")

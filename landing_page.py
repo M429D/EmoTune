b75547a5cecb798e22400bb0955ab17eec1fa6f2
M429D <92875016+M429D@users.noreply.github.com>
2024-11-03 01:12:49 +0800

    Update landing_page.py

--- landing_page.py
+++ landing_page.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import gspread
+import time
 
 # Get the payment status from URL query parameters
 payment_status = st.query_params.get("payment", [""])  # 'success' or 'fail'
@@ -7,7 +7,19 @@ payment_status = st.query_params.get("payment", [""])  # 'success' or 'fail'
 # Display the appropriate message based on payment status
 if payment_status == "success":
     st.success("Thank you for your payment! Your order has been successfully processed.")
+    countdown = 10  # Countdown time in seconds
+    for i in range(countdown, 0, -1):
+        st.write(f"Closing this tab in {i} seconds...")
+        time.sleep(1)  # Sleep for 1 second
+    st.write('<script>window.close();</script>', unsafe_allow_html=True)
+
 elif payment_status == "fail":
     st.error("Payment failed. Please try again or contact support.")
+    countdown = 10  # Countdown time in seconds
+    for i in range(countdown, 0, -1):
+        st.write(f"Closing this tab in {i} seconds...")
+        time.sleep(1)  # Sleep for 1 second
+    st.write('<script>window.close();</script>', unsafe_allow_html=True)
+
 else:
     st.warning("No payment status provided.")

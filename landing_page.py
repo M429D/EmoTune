c021b458d315f5517869f2c8a462b08dedf64a36
M429D <92875016+M429D@users.noreply.github.com>
2024-11-03 15:12:18 +0800

    Update landing_page.py

--- landing_page.py
+++ landing_page.py
@@ -7,9 +7,9 @@ payment_status = st.query_params.get("payment", [""])  # 'success' or 'fail'
 # Display the appropriate message based on payment status
 if payment_status == "success":
     st.success("Payment success. Thank you for your patronage!")
-    st.write(f"Please close this tab and verify on our website that payment has been made to confirm your transaction and view order invoice.")
+    st.write(f"Please close this tab and verify on Twilight CS's website that payment has been made to confirm your transaction and view order invoice.")
 elif payment_status == "fail":
     st.warning("Payment failed!")
-    st.write(f"Please close this tab and verify on our website that payment has failed in order to try again.")
+    st.write(f"Please close this tab and verify on Twilight CS's website that payment has failed in order to try again.")
 else:
     st.warning("No payment status provided.")

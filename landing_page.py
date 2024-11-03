a3c4c0c13fd7e0914f7e181684611364c3bcbbaf
M429D <92875016+M429D@users.noreply.github.com>
2024-11-03 15:14:29 +0800

    Update landing_page.py

--- landing_page.py
+++ landing_page.py
@@ -9,7 +9,7 @@ if payment_status == "success":
     st.success("Payment success. Thank you for your patronage!")
     st.write(f"Please close this tab and verify on Twilight CS's website that payment has been made to confirm your transaction and view order invoice.")
 elif payment_status == "fail":
-    st.warning("Payment failed!")
+    st.error("Payment failed!")
     st.write(f"Please close this tab and verify on Twilight CS's website that payment has failed in order to try again.")
 else:
     st.warning("No payment status provided.")

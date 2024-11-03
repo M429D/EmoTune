960b986467683da971ae4039f2dc913264bbcd71
M429D <92875016+M429D@users.noreply.github.com>
2024-11-03 13:56:02 +0800

    Update landing_page.py

--- landing_page.py
+++ landing_page.py
@@ -7,13 +7,6 @@ payment_status = st.query_params.get("payment", [""])  # 'success' or 'fail'
 # Display the appropriate message based on payment status
 if payment_status == "success":
     st.success("Payment success. Thank you for your patronage!")
-   
-    countdown_placeholder = st.empty()  # Create a placeholder for the countdown
-    countdown_time = 30  # Start countdown from 7 seconds
-
-    # Countdown loop
-    for i in range(countdown_time, 0, -1):
-        countdown_placeholder.write(f"Please close this tab and click 'I have paid' on our website to confirm your transaction within {i} seconds...")
-        time.sleep(1)  # Sleep for 1 second
+    st.write(f"Please close this tab and verify on our website that payment has been made to confirm your transaction.")
 else:
     st.warning("No payment status provided.")
